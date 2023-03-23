495c3e2d64422263f4eeb2cd4812dbd714d3c9cb
yukgakmon <69315573+horaemong@users.noreply.github.com>
2023-03-23 19:06:10 +0900

    Update 과제1.c

--- 2주차/과제1.c
+++ 2주차/과제1.c
@@ -1,23 +1,23 @@
-// #include <stdio.h>
+//#include <stdio.h>
 //
-// void main()
+//void main()
 //{
 //	double f_temp;
 //	double c_temp;
 //
-//	// ?? ?? ???
-//	printf("?? ??? ????? : ");
+//	// 화씨 온도 구하기
+//	printf("화씨 온도를 입력하세요 : ");
 //	scanf_s("%lf", &f_temp);
 //
 //	c_temp = (f_temp - 32) * 5 / 9;
 //
-//	printf("?? %.2f?? ?? %.2f????.\n", f_temp, c_temp);
-//
-//	// ?? ?? ???
-//	printf("?? ??? ????? : ");
+//	printf("화씨 %.2f도는 섭씨 %.2f도입니다.\n", f_temp, c_temp);
+//	
+//	// 섭씨 온도 구하기
+//	printf("섭씨 온도를 입력하세요 : ");
 //	scanf_s("%lf", &c_temp);
 //
 //	f_temp = c_temp * 9 / 5 + 32;
 //
-//	printf("?? %.2f?? ?? %d????.\n", c_temp, (int)f_temp);
-// }
+//	printf("섭씨 %.2f도는 화씨 %d도입니다.\n", c_temp, (int)f_temp);
+//}
